8e2dabf8a79834dc6bc2435ae7a7e87947c6266b
Luke0195 <lucas201273@gmail.com>
2020-10-04 18:17:32 -0300

    feat: menu da aplicaçao

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
 import { Nav } from './styled';
 
 export default function Header() {
   return (
     <Nav>
       <a href="#" target="_blank">
-        Home
+        <FaHome size={24} />
       </a>
       <a href="#" target="_blank">
-        Teste{' '}
+        <FaUserAlt size={24} />
+      </a>
+      <a href="#" target="_blank">
+        <FaSignInAlt size={24} />
       </a>
     </Nav>
   );
