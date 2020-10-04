a7ed060e57081d2b2cd92a804221a2aea31bc9c8
Luke0195 <lucas201273@gmail.com>
2020-10-04 12:14:20 -0300

    chore: add .env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function App() {
-  return <h1> Desenvolvimento da aplicação web </h1>;
+  return (
+    <>
+      <h1> Desenvolvimento da aplicação web </h1>
+      <h2> Teste </h2>
+    </>
+  );
 }
